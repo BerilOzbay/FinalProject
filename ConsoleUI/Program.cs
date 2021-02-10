dcaec3311b1b80648f98466504c166ce111fdb62
Beril Özbay <beril_ozbay@hotmail.com>
2021-02-10 15:36:41 +0300

    Dtos

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
@@ -8,11 +9,26 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductTest();
+            //CategoryTest();
+        }
 
-            foreach(var product in productManager.GetAll())
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
---
+++ Entities/Concrete/Order.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Order: IEntity
+    {
+        public int OrderId { get; set; }
+        public int EmployeeId { get; set; }
+        public string CustomerId { get; set; }
+        public string ShipCity { get; set; }
+        public DateTime OrderTime { get; set; }
+    }
+}
